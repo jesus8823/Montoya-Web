50c1049d8eebe9176e01fd200228a56c8efe576c
jesus8823 <jesusmolina8823@gmail.com>
2024-03-24 07:58:55 -0300

    version 1, inicio de aplicacion

---
+++ codigo/menu.js
@@ -0,0 +1,38 @@
+window.addEventListener('scroll', function() {
+  const menu = document.getElementById('menu');
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 0) {
+    menu.classList.add('scrolled');
+  } else {
+    menu.classList.remove('scrolled');
+  }
+});
+
+// console.log('Hola mundo! es mi primera linea en JS');
+      const ipad = window.matchMedia('screen and (max-width: 767px)')
+      const menu = document.querySelector('.menu');
+      // console.log(menu);
+      const burgerButton = document.querySelector('#burger-menu');
+      // console.log(burgerButton);
+
+
+      ipad.addListener(validation)
+
+      function validation(event) {
+        if (event.matches) {
+          burgerButton.addEventListener('click', hideShow)
+        } else {
+          burgerButton.removeEventListener('click', hideShow)
+        }
+      }
+      validation(ipad);
+
+
+      function hideShow() {
+        if (menu.classList.contains('is-active')) {
+          menu.classList.remove('is-active');
+        } else {
+          menu.classList.add('is-active');
+        }
+      }
